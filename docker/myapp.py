4d7bfaf0476bcf36b691331a7c86cb11e8b962e8
gbhure <45383300+gbhure@users.noreply.github.com>
2022-03-19 22:17:49 +0530

    Update and rename http_serv.py to myapp.py

---
+++ docker/myapp.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+import os
+
+app = Flask(__name__)
+@app.route('/')
+
+def hello():
+    return('Hello from container..\n')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)

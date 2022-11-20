8d5e890f5d0419a282663c0775d81cc264dbd17c
gbhure <45383300+gbhure@users.noreply.github.com>
2022-11-20 19:19:54 +0530

    Update myapp.py

--- docker/myapp.py
+++ docker/myapp.py
@@ -1,12 +1,11 @@
 from flask import Flask
-
 import os
 
 app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    return('Hello from container..\n')
+    return('Hello from container..This is my first docker container\n')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
